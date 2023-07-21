dd012d82b6c7a0a605fafaba962e702f647b4502
OmAR <omarsteven2398@gmail.com>
2023-07-22 00:00:05 +0300

    counter_4

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutters/Login.dart';
 import 'package:flutters/home_screen.dart';
 import 'package:flutters/messanger.dart';
 import 'package:flutters/user_screen.dart';
+import 'counter_screen.dart';
 
 void main() {
   runApp(omar());
@@ -16,7 +16,7 @@ class omar extends StatelessWidget
   Widget build(BuildContext context) {
       return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home : userScreen(),
+        home : statefull1(),
       );
   }
   
